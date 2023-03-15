b8b2ba8a3e88d89387ca9bceb748bab5702d00de
anthony29p <anthony.perez@uni.pe>
2023-03-15 18:06:46 -0500

    red-green-reflect-refactor

--- rock-paper-scissors.test.ts
+++ rock-paper-scissors.test.ts
@@ -1,16 +1,33 @@
-import { createRockPaperScissor } from "./rock-paper-scissors"
+import { createRockPaperScissor, Move, Result } from "./rock-paper-scissors"
 
 describe('Test de prueba', () =>{
-    test('it must work', () => {
+    test('given player move paper and opponent move rock', () => {
         // Arrange (arreglar)
-        const expected = 'Player Wins'
+        const expected = Result.playerWins
         const sut = createRockPaperScissor()
-
         // Act
-        const actual = sut.play()
-
+        const actual = sut.play(Move.Paper,Move.Rock)
         // Assert (afirmar)
-        expect(expected).toBe(actual)
+        expect(actual).toBe(expected)
+    })
 
+    test('given player move paper and opponent move scissors', () => {
+        // Arrange
+        const expected = Result.playerLoses
+        const sut = createRockPaperScissor()
+        // Act
+        const actual = sut.play(Move.Paper,Move.Scissors)
+        // Assert
+        expect(actual).toBe(expected)
+    })
+
+    test('given player move paper and opponent move paper', () => {
+        // Arrange
+        const expected = Result.Tie
+        const sut = createRockPaperScissor()
+        // Act
+        const actual = sut.play(Move.Paper,Move.Paper)
+        // Assert
+        expect(actual).toBe(expected)
     })
 })
--- rock-paper-scissors.ts
+++ rock-paper-scissors.ts
@@ -1,7 +1,29 @@
-export const createRockPaperScissor = () => {
+export enum Move {
+    Paper,
+    Rock,
+    Scissors
+}
+
+export enum Result {
+    playerWins,
+    playerLoses,
+    Tie
+}
+
+interface RockPaperScissor {
+    play(p1: Move, p2: Move) : Result
+}
+
+export const createRockPaperScissor = ():RockPaperScissor => {
     return {
-        play (){
-            return ('Player Wins')
+        play (p1:Move, p2:Move){
+            if(p2 === Move.Scissors){
+                return (Result.playerLoses)
+            }
+            else if (p2 === Move.Paper){
+                return (Result.Tie)
+            }
+            return (Result.playerWins)
         }
     }  
 }
